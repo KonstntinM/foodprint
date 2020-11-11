074707602086a02ca99d7adbfa25bebab5471d2b
Konstantin <k_marx@gmx.de>
2020-11-11 20:10:51 +0100

    add animation to scanner screen

--- src/screens/ScannerScreen.js
+++ src/screens/ScannerScreen.js
@@ -2,10 +2,9 @@ import { Layout } from '@ui-kitten/components';
 import React, { Component } from 'react';
 
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 import Header from '../components/base/header'
-
-
 import BarcodeScanner from '../components/BarcodeScanner'
 
 
@@ -18,6 +17,16 @@ export default class ScannerScreen extends Component {
         <Layout style={styles.body}>
           <BarcodeScanner style={styles.scanner} />
         </Layout>
+        <View style={styles.info}>
+          <View style={styles.animation}>
+              <LottieView source={require('../../assets/animations/world-loader.json')} autoPlay loop/>  
+          </View>
+          <View style={styles.textBox}>
+                <Text style={styles.text}>Start scanning now!</Text>
+                <Text style={styles.text}>Thanks for using Foodprint. With your commitment you help to protect our planet.</Text>
+          </View> 
+        </View>
+
       </SafeAreaView>
     )
   }
@@ -32,9 +41,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 15 
+    marginTop: 15,
   },
   scanner: {
     marginTop: 10
+  },
+  info: {
+    flexDirection: 'row'
+  },
+  animation: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  text: {
+    textAlign: 'right',
+  },
+  textBox: {
+    flexDirection: 'column'
   }
 });
